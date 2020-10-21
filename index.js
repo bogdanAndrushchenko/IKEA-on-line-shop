32c10226cb14a2bb425bb67f209972b8d6628602
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-10-21 19:42:25 +0300

    corect styles and any markup

---
+++ index.js
@@ -0,0 +1,14 @@
+
+import generateHeader from './scripts/generateHeader.js';
+import generateFooter from './scripts/generateFooter.js';
+import generateCatalog from './scripts/generateCatalog.js';
+import generateGoodsPage from './scripts/generateGoodsPage.js'
+
+import {loadData} from './scripts/loadData.js';
+
+generateHeader();
+generateFooter();
+generateCatalog();
+generateGoodsPage();
+loadData();
+
